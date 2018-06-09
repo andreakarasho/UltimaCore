f7a93016b17ef8c65faa8093bb6ad2a2b2222e26
Andrea Angelini <andrea-angelini@outlook.it>
2018-06-09 11:13:32 +0200

    + light

---
+++ UltimaCore/Graphics/Light.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace UltimaCore.Graphics
+{
+    public static class Light // is it useful?
+    {
+        private static UOFileMul _file;
+
+        public static void Load()
+        {
+            string path = Path.Combine(FileManager.UoFolderPath, "light.mul");
+            string pathidx = Path.Combine(FileManager.UoFolderPath, "lightidx.mul");
+
+            if (!File.Exists(path) || !File.Exists(pathidx))
+                throw new FileNotFoundException();
+
+            _file = new UOFileMul(path, pathidx, 100);
+        }
+    }
+}
