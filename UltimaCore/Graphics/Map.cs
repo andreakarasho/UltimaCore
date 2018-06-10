fdb516be0a5a36b983e1383d278395e96eea9f05
Andrea Angelini <andrea-angelini@outlook.it>
2018-06-10 19:11:54 +0200

    +

---
+++ UltimaCore/Graphics/Map.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UltimaCore.Graphics
+{
+    public class Map : IDisposable
+    {
+        private TileMatrix _tiles;
+
+        public Map(int map, int width, int height)
+        {
+            Index = map; Width = width; Height = height;
+        }
+
+        public int Index { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
